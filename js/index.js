34093be2da73a5212bdc505f74fea0d0366659f7
DHidalgoJ <33376224+DHidalgoJ@users.noreply.github.com>
2020-12-04 14:20:22 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const initializeSkills = () => {
+    let skills = document.getElementsByClassName('skill');
+    for (const skill of skills) {
+        let percent = skill.getAttribute('data-percent')
+        skill.getElementsByClassName('skillbar-progress')[0].style.width = percent + '%'
+    }
+}
+
+initializeSkills();
